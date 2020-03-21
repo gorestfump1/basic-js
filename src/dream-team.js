2fab857cd26e1a580311387fb2646b14f070549c
Andrei <58465851+gorestfump1@users.noreply.github.com>
2020-03-21 15:03:53 +0300

    fix solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,7 +1,7 @@
 module.exports = function createDreamTeam(friends) {
- if(typeof !== 'string'){
-      return ("false")
-    } else {
+ if(typeof === 'string'){
       return friends.map(word => word.slice(0, 1).toUpperCase()).sort().join("");
+    } else {
+     return false;
     }
   };
