0f2582f84bfa3e35dd9e84420120c516591c8105
Andrei <58465851+gorestfump1@users.noreply.github.com>
2020-03-22 09:55:02 +0300

    fix  solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,34 +1,10 @@
 module.exports = function getSeason(date) {
   
-  if(!date){
-    return 'Unable to determine the time of year!'
-    }
-    else if(date.hasOwnProperty('getMonth')) {
-    throw Error
-    }
-  
+  if(!arguments[0]) return "Unable to determine the time of year!";
+  if(!(date instanceof Date && !isNaN(date.valueOf()))) throw "Error";
   let month = date.getMonth();
-       switch(month) {
-           case '11':
-           case '0':
-           case '1':
-               season = 'winter';
-           break;
-           case '2':
-           case '3':
-           case '4':
-               season = 'spring';
-           break;
-           case '5':
-           case '6':
-           case '7':
-               season = 'summer';
-           break;
-           case '8':
-           case '9': 
-           case '10':
-               season = 'fall';
-           break;
-       }
-   }
+  if(month < 2 || month === 11) return "winter";
+  if(month < 5) return "spring";
+  if(month < 8) return "summer";
+  if(month < 11) return "autumn";
 };
